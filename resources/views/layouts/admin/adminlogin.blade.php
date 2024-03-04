5706b6625ad310f98a6ef5d24d40ffbbddb3c9f5
Aatman <aatmankacha@gmail.com>
2024-03-04 23:49:32 +0530

    Modified login page design.

--- resources/views/layouts/admin/adminlogin.blade.php
+++ resources/views/layouts/admin/adminlogin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head runat="server">
     <title>Login</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
@@ -10,18 +11,19 @@
     <link href="img/favicon.ico" rel="icon" />
 
     <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Nunito:600,700" rel="stylesheet" /> 
-    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Nunito:600,700" rel="stylesheet" />
+
     <!-- CSS Libraries -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
     <link href="lib/animate/animate.min.css" rel="stylesheet" />
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
     <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
-   
+
     <script src="js/Script.js"></script>
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <form action="{{ route('getData') }}" method="post" enctype="multipart/form-data">
         @csrf <!-- CSRF Token -->
@@ -29,7 +31,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-2"></div>
                 <div class="col-lg-6 col-md-8 login-box">
-                    <div class="col-lg-12 login-key">
+                    <div class="col-lg-12 login-key mt-3">
                         <i class="fa fa-key" aria-hidden="true"></i>
                     </div>
                     <div class="col-lg-12 login-title">
@@ -39,14 +41,16 @@
                         <div class="col-lg-12 login-form">
                             <div class="form-group">
                                 <label class="form-control-label">Email</label>
-                                <input type="email" name="adminEmail" id="adminEmail" class="form-control" value="{{ old('adminEmail') }}" required="required">
+                                <input type="email" name="adminEmail" id="adminEmail" class="form-control"
+                                    value="{{ old('adminEmail') }}" required="required">
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Password</label>
-                                <input type="password" name="adminPassword" id="adminPassword" class="form-control" required="required">
+                                <input type="password" name="adminPassword" id="adminPassword" class="form-control"
+                                    required="required">
                             </div>
-                            <div class="col-lg-12 loginbttm">
-                                <div class="col-lg-6 login-btm login-text">
+                            <span>
+                                <div class="login-btm login-text pb-4">
                                     @error('adminEmail')
                                         {{ $message }}
                                     @enderror
@@ -54,10 +58,12 @@
                                         {{ $message }}
                                     @enderror
                                 </div>
-                                <div class="col-lg-6 login-btm login-button">
-                                    <button type="submit" class="btn btn-primary">LOGIN</button>
-                                </div>
+                            </span>
+
+                            <div class="text-center pb-3">
+                                <button type="submit" class="btn btn-warning w-50">LOGIN</button>
                             </div>
+
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-2"></div>
@@ -66,4 +72,5 @@
         </div>
     </form>
 </body>
+
 </html>
